31a0383b6d731dccd7647cba3828398d233b5197
vl.ostankovich <vl.ostankovich@innopolis.ru>
2019-02-27 07:02:00 +0000

    BDD 2000 images trained in Innopolis aspect ratio format (i.e. 2.5) for 55 epochs

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 train.py --dataroot ./datasets/day2night --name day2night_cyclegan --model cycle_gan
+python3 train.py --dataroot ./datasets/day2night --name day2night_bddinno_cyclegan --model cycle_gan
